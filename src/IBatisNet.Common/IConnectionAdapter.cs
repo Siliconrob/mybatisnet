ac17fe0ddb35a33a446666924062aab2e82c7548
Chris Hynes <chris@programcsharp.com>
2016-09-16 20:36:00 -0700

    Add back IConnectionAdapter
    Add .gitignore

---
+++ src/IBatisNet.Common/IConnectionAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace IBatisNet.Common
+{
+    public interface IConnectionAdapter
+    {
+        IDbConnection Adapt(IDbConnection cn);
+    }
+}
